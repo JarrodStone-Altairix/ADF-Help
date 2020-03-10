05e98f3f556e1903eb9fb53550f4cc137e3785fc
Jarrod Stone <41911539+JarrodStone-Altairix@users.noreply.github.com>
2020-03-10 09:36:40 -0400

    Created package utils

---
+++ ix/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ix',
+    version='0.1',
+    url='https://github.com/JarrodStone-Altairix/ix-util',
+    license='MIT',
+    author='Jarrod Stone',
+    author_email='jarrod.stone@altairix.com',
+    description='Common utilities package',
+    packages=find_packages(exclude=['tests']),
+    long_description=open('README.md').read(),
+    zip_safe=False
+)
